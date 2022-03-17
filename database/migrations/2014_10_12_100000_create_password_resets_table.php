a489d5dcf18a79dea58f773c4e5495d5ce143b52
alexmurithi <gitsaleks@gmail.com>
2022-03-17 16:30:10 +0300

    sql require no primary key on password resets migration

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+DB::statement('SET SESSION sql_require_primary_key=0');
+
 class CreatePasswordResetsTable extends Migration
 {
     /**
