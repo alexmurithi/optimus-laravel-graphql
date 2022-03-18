d80a516dab51a7d7d77d0b67753e3f9aa3667afa
alexmurithi <gitsaleks@gmail.com>
2022-03-18 10:59:12 +0300

    graphql tracing provider

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        \Nuwave\Lighthouse\Tracing\TracingServiceProvider::class,
 
         /*
          * Application Service Providers...
