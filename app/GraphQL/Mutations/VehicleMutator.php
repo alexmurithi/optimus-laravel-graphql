24b7f6317c147f9eaa636ce2835377a51680c959
alexmurithi <gitsaleks@gmail.com>
2022-03-18 21:48:34 +0300

    register new vehicle mutation and resolver

---
+++ app/GraphQL/Mutations/VehicleMutator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\Vehicle;
+use GraphQL\Type\Definition\ResolveInfo;
+use Illuminate\Support\Arr;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class VehicleMutator
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        return $this;
+    }
+
+    public function store($_,$args, GraphQLContext $context, ResolveInfo $resolveInfo){
+        return Vehicle::create([
+            "user_id"=>auth()->user()->id,
+            "registration_no"=>$args["registration_no"],
+            "year_of_manufacture"=>$args["year_of_manufacture"],
+            "type"=>$args["type"],
+            "tonnage"=>$args["tonnage"]
+        ]);
+    }
+}
