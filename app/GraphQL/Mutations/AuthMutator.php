c58fca0f61dacb75f261f33646c67adb9dd988cb
alexmurithi <gitsaleks@gmail.com>
2022-03-18 16:03:27 +0300

    implemented register new User mutation and schema

--- app/GraphQL/Mutations/AuthMutator.php
+++ app/GraphQL/Mutations/AuthMutator.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\GraphQL\Mutations;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
 use GraphQL\Type\Definition\ResolveInfo;
@@ -14,7 +15,7 @@ class AuthMutator
      * @param  array<string, mixed>  $args
      */
 
-
+    # function to login a user
     public function login($_, array $args,GraphQLContext $context, ResolveInfo $resolveInfo){
         $credentials =Arr::only($args,['email','password']);
 
@@ -29,6 +30,7 @@ class AuthMutator
 
     }
 
+    # function to logout out user
     public function logout($_,$args, GraphQLContext $context, ResolveInfo $resolveInfo){
         try {
             auth()->logout();
@@ -41,6 +43,18 @@ class AuthMutator
 
     }
 
+    #function to add a new user to the database
+    public function registerUser($_,$args, GraphQLContext $context, ResolveInfo $resolveInfo){
+        $inputs = Arr::only($args,['username','email','password','name']);
+        try{
+            return User::create($inputs);
+        }catch(Throwable $exception){
+            report($exception);
+        }
+
+   }
+
+   #function to return the current logged user
     public function me ($_,$args, GraphQLContext $context, ResolveInfo $resolveInfo){
         try{
             return auth()->user();
@@ -50,12 +64,14 @@ class AuthMutator
 
     }
 
+    #function to refresh access token
     public function refresh()
     {
         return $this->respondWithToken(auth()->refresh());
     }
 
 
+    #function to return logged user with access token
     protected function respondWithToken(string $token){
         return [
             "success"=>true,
